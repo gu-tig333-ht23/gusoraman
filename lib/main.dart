53a5bfa2736d98890f328e80c3217f5a04bc68b9
Antpapa99 <anthony.oram@outlook.com>
2023-09-29 16:54:34 +0200

    Final version

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'todo_list.dart';
 import 'apiproviders.dart';
 import 'theme.dart';
 
+//Om inte nyckeln fungerar gå du till filen getapikeyhereifneeded.dart för hämta en ny och sedan byter bara ut variabeln
+//apiKey på apiproviders.dart med den nya nyckel printas i debug consolen
 
 // själva programmetskärna
 void main() {
